3d09a1d02e76f0a0843b43a3baa120f6db19498e
Tonimir Kisasondi <kisasondi@gmail.com>
2020-05-24 00:25:15 +0200

    fixes

--- cveid2desc.py
+++ cveid2desc.py
@@ -2,19 +2,14 @@
 # cat nvdcve-1.1-2020.json | jq '.CVE_Items[].cve'   | jq -r '{(.CVE_data_meta.ID):(.description.description_data[].value)}'
 
 import json
+import glob
+
+nvdpath = "./nvds/*.json"  # just dump all json files in nvds folder
 
 
 def extract_descriptions():
     id_desc_dataset = {}
-    files = [
-        "nvdcve-1.1-2016.json",
-        "nvdcve-1.1-2017.json",
-        "nvdcve-1.1-2018.json",
-        "nvdcve-1.1-2019.json",
-        "nvdcve-1.1-2020.json",
-        "nvdcve-1.1-2010.json",
-    ]
-    for file in files:
+    for file in glob.glob(nvdpath):
         CVE = json.loads(open(file).read())
         for CVE_Item in CVE["CVE_Items"]:
             id = CVE_Item["cve"]["CVE_data_meta"]["ID"]
@@ -25,15 +20,7 @@ def extract_descriptions():
 
 def extract_patches():
     id_patch_dataset = {}
-    files = [
-        "nvdcve-1.1-2016.json",
-        "nvdcve-1.1-2017.json",
-        "nvdcve-1.1-2018.json",
-        "nvdcve-1.1-2019.json",
-        "nvdcve-1.1-2020.json",
-        "nvdcve-1.1-2010.json",
-    ]
-    for file in files:
+    for file in glob.glob(nvdpath):
         CVE = json.loads(open(file).read())
         for CVE_Item in CVE["CVE_Items"]:
             id = CVE_Item["cve"]["CVE_data_meta"]["ID"]
@@ -59,3 +46,11 @@ def add_info_to_file(filein, fileout, dataset_desc, dataset_patch):
             line = id + "\t" + dataset_desc[id] + "\n"
         outfile.write(line)
     outfile.close()
+
+
+if __name__ == "__main__":
+    import sys
+
+    desc = extract_descriptions()
+    patch = extract_patches()
+    add_info_to_file(sys.argv[1], sys.argv[2], desc, patch)
